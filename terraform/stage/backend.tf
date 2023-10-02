a777ea8cf12aeb5bed2fc97b446edbaa72eb3341
voytenkov <voytenkov@inbox.ru>
2023-10-03 01:22:50 +0200

    fix to old terraform

--- terraform/stage/backend.tf
+++ terraform/stage/backend.tf
@@ -1,8 +1,10 @@
-  backend "s3" {
-    endpoint                    = "storage.yandexcloud.net"
-    bucket                      = "s3-otus-devops-infra-tfstate"
-    region                      = "ru-central1-a"
-    key                         = "terraform/otus-devops-infra.tfstate"
-    skip_region_validation      = true
-    skip_credentials_validation = true
+  terraform {
+    backend "s3" {
+      endpoint                    = "storage.yandexcloud.net"
+      bucket                      = "s3-otus-devops-infra-tfstate"
+      region                      = "ru-central1-a"
+      key                         = "terraform/otus-devops-infra.tfstate"
+      skip_region_validation      = true
+      skip_credentials_validation = true
+    }
   }
