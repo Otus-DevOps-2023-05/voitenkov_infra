d2dcbde72cc158a17933d5ab3f39ad348a469d18
voytenkov <voytenkov@inbox.ru>
2023-10-08 23:49:22 +0200

    dynamic inventory tested

--- terraform/stage/main.tf
+++ terraform/stage/main.tf
@@ -2,7 +2,7 @@ module "db" {
   source           = "../modules/db"
   public_key_path  = var.public_key_path
   private_key_path = var.private_key_path
-  image_id   = var.db_disk_image
+  image_id         = var.db_disk_image
   subnet_id        = yandex_vpc_subnet.app-subnet.id
 }
 
@@ -13,5 +13,5 @@ module "app" {
   image_id         = var.app_disk_image
   subnet_id        = yandex_vpc_subnet.app-subnet.id
   mongo_ip         = module.db.internal_ip_address_db
-#  depends_on       = [module.db]
+  depends_on       = [module.db]
 }
