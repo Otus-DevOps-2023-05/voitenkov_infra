bcff3515c84ff31b25542317a2ec05a390998786
voytenkov <voytenkov@inbox.ru>
2023-10-13 13:50:49 +0200

    terraform added

---
+++ terraform/modules/db/variables.tf
@@ -0,0 +1,15 @@
+variable "public_key_path" {
+  description = "Path to the public key used for ssh access"
+}
+
+variable "private_key_path" {
+  description = "Path to the public key used for ssh access"
+}
+
+variable "image_id" {
+  description = "Disk image"
+}
+
+variable "subnet_id" {
+  description = "Subnet"
+}
