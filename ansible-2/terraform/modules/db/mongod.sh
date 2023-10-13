9d40caf2b25fbb50c68cb2b535a77ee79322edfc
voytenkov <voytenkov@inbox.ru>
2023-10-13 16:46:53 +0200

    fixes for tests 2

--- ansible-2/terraform/modules/db/mongod.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-sudo rm /etc/mongod.conf
-sudo cp -f /tmp/mongod.conf /etc/mongod.conf
-sudo chown -R mongodb:mongodb /var/lib/mongodb
-sudo systemctl restart mongod
--- ansible-2/terraform/storage-bucket.tf
+++
@@ -1,1 +0,0 @@
-# s3-bucket for terraform state file has been created initially in infrastructure/02-cloud/main.tf
